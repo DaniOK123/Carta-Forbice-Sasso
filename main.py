20e1154b2ce5d098dbde1ccf1eafdbca8c89d2e8
DaniOK123 <daniellopresti@icloud.com>
2026-05-25 21:52:11 +0200

    Inizio

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+def game():
+
+    while True:
+
+        print('Benvenuti alla nuova sfida di carta, forbice, sasso')
+        scelta = input('Vuoi iniziare un nuovo round? (y/n): ')
+        
