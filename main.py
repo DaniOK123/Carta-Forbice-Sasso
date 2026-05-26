e6f5286a407deefe60347c40b61ef97b88f564f9
DaniOK123 <daniellopresti@icloud.com>
2026-05-26 20:23:25 +0200

    miglioramenti

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def game():
     punteggio_computer = 0
 
     print('Benvenuti alla nuova sfida di carta, forbice, sasso')
+    
 
     while True:
 
@@ -20,6 +21,14 @@ def game():
         elif scelta == 'y':
 
             while True:
+                       
+                if punteggio_mio == 5:
+                    print("Hai vinto la partita!!!")
+                    return
+
+                elif punteggio_computer == 5:
+                    print("Hai perso la partita!!!")
+                    return
             
                 risultato = f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer"
                 print(risultato)
@@ -36,53 +45,61 @@ def game():
 
                     if scelta == caso:
                         print("Pareggio!")
-                        punteggio_mio += 1
-                        punteggio_computer += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
 
                     elif scelta == 'carta' and caso == 'forbice':
                         print('Hai perso!')
                         punteggio_computer += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
 
                     elif scelta == 'carta' and caso == 'sasso':
                         print('Hai vinto!')
                         punteggio_mio += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
 
                     elif scelta == 'forbice' and caso == 'carta':
                         print('Hai vinto!')
                         punteggio_mio += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
 
                     elif scelta == 'forbice' and caso == 'sasso':
                         print('Hai perso!')
                         punteggio_computer += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
 
                     elif scelta == 'sasso' and caso == 'carta':
                         print('Hai perso!')
                         punteggio_computer += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
+                        
                     
                     elif scelta == 'sasso' and caso == 'forbice':
                         print('Hai vinto!')
                         punteggio_mio += 1
                         print(f"Il risultato è: io - {punteggio_mio} : {punteggio_computer} - computer")
-                        break
                         
+                        
+                    
+                    if punteggio_mio > punteggio_computer:
+                        print("Stai vincendo!")
+                        break
+                    
+                    elif punteggio_mio < punteggio_computer:
+                        print("Stai perdendo!")
+                        break
 
+                    elif punteggio_mio == punteggio_computer:
+                        print("Stai pareggiando!")
+                        break
+            
         else:
             print('Deve essere una scelta tra y e n')
 
         
 
-  
 game()
-
